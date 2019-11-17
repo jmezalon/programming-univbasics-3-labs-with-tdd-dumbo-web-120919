de04a07fedb8531f03c858a5c5229213561bd552
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-17 10:23:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ second_number = 1
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-
+quotient = first_number / second_number
 
 # def Add (first_number, second_number)
 #   sum = first_number + second_number
