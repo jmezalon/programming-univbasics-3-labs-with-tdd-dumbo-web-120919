802c63c158482aae3bf69e489e805e648148edab
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-17 10:18:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,10 @@
 # Add your variables here
 first_number = 0
 second_number = 1
-def Add (first_number, second_number)
+def sum (first_number, second_number)
   first_number + second_number
+end
+
+def difference(first_number, second_number)
+  second_number - first_number
 end
