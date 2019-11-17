a6d613d8653fcaca5dd197a88593d07dc3a43106
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-17 10:13:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-# first_number = 0
-# second_number = 1
+first_number = 0
+second_number = 1
 def Add (first_number, second_number) do
   first_number + second_number
 end
